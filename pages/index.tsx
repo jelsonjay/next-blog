f43c302d722497b5d818d4ec2f332c2389dd6c69
Jelson J <jelson20@outlook.com>
2021-08-10 20:56:54 +0100

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,12 +13,12 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.tsx!</a>
         </h1>
 
         <p className={styles.description}>
           Get started by editing{' '}
-          <code className={styles.code}>pages/index.js</code>
+          <code className={styles.code}>pages/index.tsx</code>
         </p>
 
         <div className={styles.grid}>
